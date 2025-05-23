09b5d69e4aee9479eebabe9c08cee605e47ceac3
sumaiyadewanmim <sumaiyadewanmim@gmail.com>
2025-05-24 01:04:19 +0600

    nestedLoops

---
+++ doWhileLoops.js
@@ -0,0 +1,18 @@
+//Do While Loops
+
+var isRunning = false;
+while (isRunning) {
+  console.log("I am running");
+}
+
+do {
+  console.log("i am Sumaiya Dewan Mim");
+} while (isRunning);
+
+//New Code
+
+let count = 0;
+do {
+  console.log("I Love JavaScript");
+  count++;
+} while (count <= 1);
---
+++ nestedLoops.js
@@ -0,0 +1,35 @@
+// Nested Loop
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+//For Number
+// var n = 20;
+// for (var i = 1; i <= n; i++) {
+//   var result = "";
+//   for (var j = 1; j <= i; j++) {
+//     result += j + " ";
+//   }
+//   console.log(result);
+// }
+
+//For Star
+// var n = 20;
+// for (var i = 1; i <= n; i++) {
+//   var result = "";
+//   for (var j = 1; j <= i; j++) {
+//     result += " * ";
+//   }
+//   console.log(result);
+// }
+
+var n = 20;
+for (i = 1; i <= n; i++) {
+  var result = "";
+  for (var j = 1; j <= n; j++) {
+    result += " * ";
+  }
+  console.log(result);
+}
