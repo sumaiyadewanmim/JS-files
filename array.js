8bfbbc789545ee3e77a5985301bee1e6c6f73e12
sumaiyadewanmim <sumaiyadewanmim@gmail.com>
2025-05-27 22:48:39 +0600

    initial commit

---
+++ array.js
@@ -0,0 +1,7 @@
+//What is Array
+//Array is a Data Structure where we can organize multiple data
+// a single variable
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(arr[0]);
+console.log(arr[4]);
---
+++ stringLength.js
@@ -0,0 +1,19 @@
+// const password = "mypassword";
+// if (password.length >= 8) {
+//   console.log("Strong password ");
+// } else {
+//   console.log("Weak password ");
+// }
+
+var str = "I Choose Myself";
+var length = 0;
+
+while (true) {
+  if (str.charAt(length) == "") {
+    break;
+  } else {
+    length++;
+  }
+}
+console.log(str.length);
+console.log("Alhamdulillah".length);
